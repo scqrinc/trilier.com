ba78f4208dc5923ca1047aefabc7142549b84d8f
nabbisen <nabbisen@scqr.net>
2021-08-14 20:31:56 +0900

    add echoes as guide

--- dev-scripts/build.sh
+++ dev-scripts/build.sh
@@ -1,10 +1,16 @@
 
 cd app-wasm
+echo ====== `pwd` start ======
 wasm-pack build
+echo ====== `pwd` end ======
 cd ..
 
 cd app-web
+echo ====== `pwd` start ======
 npm run build
+echo ====== `pwd` end ======
 cd ..
 
+echo ====== `pwd` start ======
 cargo build
+echo ====== `pwd` end ======
--- dev-scripts/install.sh
+++ dev-scripts/install.sh
@@ -4,11 +4,15 @@ SCRIPTS_DIR=$(dirname "$0")
 sh $SCRIPTS_DIR/build.sh
 
 cd app-web
+echo ====== `pwd` start ======
 npm install
 npm audit fix
+echo ====== `pwd` end ======
 cd ..
 
 DOT_ENV=.env
 if ! test -f "$DOT_ENV"; then
+    echo ====== `pwd` start ======
     printf "TRANSLATE_API_KEY = (set key here)\n" > "$DOT_ENV"
+    echo ====== `pwd` end ======
 fi
