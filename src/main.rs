29b5c1b6704dbd38dc7fb332e474b1f27e507acb
nabbisen <nabbisen@scqr.net>
2021-11-20 18:05:32 +0900

    print server addr and port

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ use std::path::{Path, PathBuf};
 use dotenv;
 use serde_json::Value;
 
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Result};
 use actix_files::NamedFile;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Result};
 
 use ureq;
 
 mod env;
 
+const LISTEN_ADDR: &str = "127.0.0.1";
+const LISTEN_PORT: i32 = 8080;
 const DOCUMENT_ROOT_DIR: &str = "./app-web/dist";
 
 const TRANSLATE_API_URL: &str = "https://api-free.deepl.com/v2/translate";
@@ -21,36 +23,43 @@ async fn search(params: web::Json<env::SearchParams>) -> Result<HttpResponse> {
 
     let mut results = Vec::new();
     for dst_lang in &params.dst_langs {
-        let result = query_translate(&params.original_text, &params.src_lang, dst_lang, translate_api_key.as_ref()).await;
+        let result = query_translate(
+            &params.original_text,
+            &params.src_lang,
+            dst_lang,
+            translate_api_key.as_ref(),
+        )
+        .await;
         results.push(result);
     }
     let search_results = env::SearchResults { results: results };
     Ok(HttpResponse::Ok().json(search_results))
 }
-async fn query_translate(original_text: &str, src_lang: &str, dst_lang: &str, translate_api_key: &str) -> env::SearchResult {
+async fn query_translate(
+    original_text: &str,
+    src_lang: &str,
+    dst_lang: &str,
+    translate_api_key: &str,
+) -> env::SearchResult {
     let params = [
         ("auth_key", translate_api_key),
         ("text", original_text),
         ("source_lang", src_lang),
-        ("target_lang", dst_lang)
+        ("target_lang", dst_lang),
     ];
 
     let resp_txt = match ureq::post(TRANSLATE_API_URL).send_form(&params) {
         Ok(resp) => resp.into_string().unwrap(),
         Err(ureq::Error::Status(code, resp)) => {
             panic!("Invalid response: {} = {}", code, resp.status_text());
-        },
+        }
         Err(error) => {
             panic!("Server error: {}", error);
-        },
+        }
     };
 
     let resp_body_json: Value = serde_json::from_str(&resp_txt).unwrap();
-    let translation = resp_body_json
-        .get("translations")
-        .unwrap()
-        .get(0)
-        .unwrap();
+    let translation = resp_body_json.get("translations").unwrap().get(0).unwrap();
     let translated_text = translation.get("text").unwrap().as_str().unwrap();
     let detected_src_lang = match translation.get("detected_source_language") {
         Some(x) => x.as_str().unwrap(),
@@ -76,7 +85,9 @@ async fn static_file(req: HttpRequest) -> Result<NamedFile> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let listen_addr_port: String = format!("{}:{}", LISTEN_ADDR, LISTEN_PORT);
+    println!("Server {} running", listen_addr_port);
+    let srv = HttpServer::new(|| {
         App::new()
             .service(
                 web::scope("/api")
@@ -89,7 +100,6 @@ async fn main() -> std::io::Result<()> {
                     .route("/{filename:.*}", web::get().to(static_file)),
             )
     })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    .bind(listen_addr_port)?;
+    srv.run().await
 }
